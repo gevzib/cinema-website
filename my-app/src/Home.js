85d125fcb58b87df08c823690266d40bbcc1aad7
trainEnthusiast314 <trainsAreCool@protonmail.com>
2024-02-06 14:16:40 +0000

    error

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -3,10 +3,5 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 
 export default () => {
-  useEffect(() => {
-    axios.get("http://localhost:3000/posts").then((res) => {
-      console.log(res.data);
-    });
-  });
   return <h1>Cinema Website Homepage</h1>;
 };
