e42d7d2e1f7245c54cde4618cbb8842cdbab3034
trainEnthusiast314 <trainsAreCool@protonmail.com>
2024-02-06 14:13:19 +0000

    server

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import { useState } from 'react';
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 export default () => {
-    return (
-       <h1>Cinema Website Homepage</h1>
-    )
+  useEffect(() => {
+    axios.get("http://localhost:3000/posts").then((res) => {
+      console.log(res.data);
+    });
+  });
+  return <h1>Cinema Website Homepage</h1>;
 };
