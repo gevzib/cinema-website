a3702a8cb48d6bd1f966fbd275c662bb99d780ef
E29102 <ejr29102@gmail.com>
2024-02-07 13:42:02 +0000

    Profile update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,26 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Person from './AddFilm';
 import Home from './Home';
-import AddFilm from './AddFilm';
+
 import Films from './Films';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-import Film1 from './Film1';
-import Film2 from './Film2';
-import Film3 from './Film3';
+
+
 import Nav from './Nav';
 import Contact from './Contact';
 import ConfirmFilm from './ConfirmFilm';
 import Profile from './Profile';
 import { useState } from 'react';
 
-import Home from "./Home";
 import AddFilm from "./AddFilm";
-import Films from "./Films";
+
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
-import Nav from "./Nav";
-import Contact from "./Contact";
-import ConfirmFilm from "./ConfirmFilm";
+
+
+
 import FilmCard from "./FilmCard";
 import { createContext } from "react";
 const ThemeContext = createContext(null);
