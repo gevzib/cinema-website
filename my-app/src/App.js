c39f14f56c07712d5c2b97503e6ed1d414480fe2
trainEnthusiast314 <trainsAreCool@protonmail.com>
2024-02-07 13:51:43 +0000

    addfilm re-added to routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Person from './AddFilm';
-import Home from './Home';
+import logo from "./logo.svg";
+import "./App.css";
+import Person from "./AddFilm";
+import Home from "./Home";
 
-import Films from './Films';
+import Films from "./Films";
 
-
-import Nav from './Nav';
-import Contact from './Contact';
-import ConfirmFilm from './ConfirmFilm';
-import Profile from './Profile';
-import { useState } from 'react';
+import Nav from "./Nav";
+import Contact from "./Contact";
+import ConfirmFilm from "./ConfirmFilm";
+import Profile from "./Profile";
+import { useState } from "react";
 
 import AddFilm from "./AddFilm";
 
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-
-
-
 
 import FilmCard from "./FilmCard";
 import { createContext } from "react";
 const ThemeContext = createContext(null);
 function App() {
   let [userDetails, setUserDetails] = useState({});
-
 
   return (
     <div className="App">
@@ -36,7 +31,11 @@ function App() {
             <Route path="/films" element={<Films />} />
             <Route path="/films/:films_id" element={<FilmCard />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/profile" element={<Profile setUserDetails= {setUserDetails}/>} />
+            <Route
+              path="/profile"
+              element={<Profile setUserDetails={setUserDetails} />}
+            />
+            <Route path="/addfilm" element={<AddFilm />} />
           </Routes>
         </header>
       </BrowserRouter>
